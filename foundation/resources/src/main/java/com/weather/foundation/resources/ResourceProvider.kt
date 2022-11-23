8df6d025308141d22ad14f55885bdb486de3c307
Maksym Lytvynov <max.gen.lit@gmail.com>
2022-11-23 16:10:50 +0200

    Resource module created

---
+++ foundation/resources/src/main/java/com/weather/foundation/resources/ResourceProvider.kt
@@ -0,0 +1,17 @@
+package com.weather.foundation.resources
+
+import android.content.Context
+import androidx.annotation.StringRes
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+
+class ResourceProvider @Inject constructor(
+    @ApplicationContext private val context: Context,
+) {
+
+    fun getString(@StringRes resId: Int) =
+        context.getString(resId)
+
+    fun getString(@StringRes resId: Int, vararg args: Any?) =
+        context.getString(resId, args)
+}
